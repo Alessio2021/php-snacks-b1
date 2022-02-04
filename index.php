af446949371ea3eff70b6fbe90e616bc8c5bcdab
Alessio2021 <alessiobut1911@gmail.com>
2022-02-04 17:49:14 +0100

    fix v for

--- index.php
+++ index.php
@@ -15,27 +15,19 @@
     <div id="app">
         <div class="d-flex">
             <div v-for="car in cars" class="container">
-                <img style="order: -1" :src="car.immagine" />
-                <h3>{{car.marca}}</h3>
-                <h3>{{car.modello}}</h3>
-                <h3>{{car.cavalli}}</h3>
-                <h3>{{car.immatricolazione}}</h3>
-                <h3>{{car.cilindrata}}</h3>
-                <h3>{{car.accessori}}</h3>
-                <h3>{{car.carburante}}</h3>
-            </div>
-            <!-- <div v-for="car in cars" class="container">
-                <div v-for="element in car" class="">
-                    <div v-if="element === 'immagine'">
-                        <img style="order: -1" :src="element.immagine" />
+                <div v-for="(element, key) in car" class="">
+                    <div v-if="key == 'immagine'">
+                        <img style="order: -1" :src="element" />
                     </div>
                     <div v-else>
                         <h3>{{element}}</h3>
                     </div>
                 </div>
-            </div> -->
+            </div>
         </div>
     </div>
+
+    
 <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
 
 <script src="js/script.js"></script>
